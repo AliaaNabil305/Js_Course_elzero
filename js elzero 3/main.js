2f94004b938790e5630a5d9db8ec477b7fde9033
Aliaa Nabil <114958840+AliaaNabil305@users.noreply.github.com>
2023-09-17 19:59:24 +0300

    Update main.js

--- js elzero 3/main.js
+++ js elzero 3/main.js
@@ -38,3 +38,33 @@ console.log(+num * (true+true)); // 20
 
 // Solution Four
 console.log(++num + --num - true); // 20
+
+
+
+/* let points = 10;
+
+points+=true + true + true 
+
+console.log(points); // 13
+
+points-=true + true + true + true + true
+
+console.log(points); // 8; */
+
+/* 
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++);
+console.log(++a + -b + +c++ - -a++ + +a);
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true); */
+
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+console.log(-d * +e ); // 2000
+console.log(++e*++g  + -d + ++f); // 173
