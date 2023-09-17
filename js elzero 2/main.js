4bd5eb67d4c209bb655fe85610c75a351ea66487
Aliaa Nabil <114958840+AliaaNabil305@users.noreply.github.com>
2023-09-17 19:12:59 +0300

    Add files via upload

---
+++ js elzero 2/main.js
@@ -0,0 +1,35 @@
+//  // Add Variables Here
+// let NumberOne=10  
+// let NumberTwo=20
+
+// // Ouput
+
+// console.log(NumberOne + "" + NumberTwo);
+
+// console.log(typeof(NumberOne + "" + NumberTwo)); // Normal Concatenate => String
+// console.log(`${NumberOne}${NumberTwo}`); // Template Literals Way => 1020
+// console.log(`${typeof NumberOne}${typeof NumberTwo}`); 
+// console.log(NumberOne + "\n" + NumberTwo);
+// /*
+//   Normal Concatenate
+//   20
+//   10
+// */
+
+// console.log(`${NumberOne}
+// ${NumberTwo}`);
+// /*
+//   Template Literals Way
+//   20
+//   10
+// */
+
+
+
+// console.log("`I'm In \n \\ \n Love \\\\ \"\"\" ''' \n ++ with ++ \n \\ \"\"\" \\ \"\"\" \n \"\" javascript \"\" ``")
+
+let a = 21;
+let b = 20;
+let c = 2021
+
+console.log(`_${a}_${c}_${c}_${c}_${b}_`); // _21_2021_2021_2021_20_
